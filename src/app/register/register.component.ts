6e14726345b0ec4a5258c29adcc6c37b6db747aa
bestcoolestp <108534975+bestcoolestp@users.noreply.github.com>
2023-09-02 09:22:25 +0900

    feat: first commit

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,57 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.scss']
+// })
+// export class RegisterComponent {
+
+// }
+
+
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  firstName: string ="";
+  lastName: string ="";
+  email: string ="";
+  password: string ="";
+
+  constructor(private http: HttpClient) 
+  {
+  }
+
+  ngOnInit(): void
+  {
+  }
+
+  register()
+  {
+    let bodyData = 
+    {
+      "firstName" : this.firstName,
+      "lastName" : this.lastName,
+      "email" : this.email,
+      "password" : this.password,
+    };
+    this.http.post("http://localhost:9992/student/create",bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Student Registered Successfully")
+    });
+  }
+
+  save()
+  {
+    this.register();
+  }
+
+}
